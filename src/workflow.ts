9846ed6f366b381d56919dc8f657178fb70e77f9
John Daly <john.daly2@gmail.com>
2023-02-09 19:53:48 -0800

    First stab at a buildConcurrentWorkflow function

--- src/workflow.ts
+++ src/workflow.ts
@@ -21,6 +21,35 @@ export const makeWorkflowBuilder = <W extends UnknownWorkflowDefinition>() => {
     [Id in TaskId<W>]: Task<W, Id, TaskId<W>>
   }> = {}
 
+  const validateAndBuildGraph = () => {
+    // type cast: assume all tasks are registered
+    const taskList = Object.values(tasks) as TaskFor<W>[]
+
+    // sanity check dependencies in case TS was ignored
+    for (const task of taskList) {
+      for (const dep of task.dependencies) {
+        if (!(dep in tasks))
+          throw new Error(`Task ${task.id} has unregistered dependency ${dep}`)
+      }
+    }
+
+    // construct a graph so we can detect cycles and topo-sort
+
+    const graph = Graph()
+
+    for (const task of taskList) {
+      graph.addNode(task.id)
+      for (const dep of task.dependencies) {
+        // missing nodes are added implicitly so order doesn't matter
+        graph.addEdge(task.id, dep)
+      }
+    }
+
+    if (graph.hasCycle()) throw new Error(`Task graph has a cycle`)
+
+    return graph
+  }
+
   return {
     addTask: <Id extends TaskId<W>, DepId extends TaskId<W>>(
       task: Task<W, Id, DepId>,
@@ -34,34 +63,139 @@ export const makeWorkflowBuilder = <W extends UnknownWorkflowDefinition>() => {
       tasks[task.id] = task
     },
 
-    buildSerialWorkflow: (options: { selectedTasks?: TaskId<W>[] } = {}) => {
-      // type cast: assume all tasks are registered
-      const taskList = Object.values(tasks) as TaskFor<W>[]
+    buildConcurrentWorkflow: (
+      options: { selectedTasks?: TaskId<W>[] } = {},
+    ) => {
+      const graph = validateAndBuildGraph()
+      const taskOrder: TaskId<W>[] = graph
+        .topologicalSort(options.selectedTasks, true)
+        .reverse()
 
-      // sanity check dependencies in case TS was ignored
-      for (const task of taskList) {
-        for (const dep of task.dependencies) {
-          if (!(dep in tasks)) {
-            throw new Error(
-              `Task ${task.id} has unregistered dependency ${dep}`,
-            )
+      // done with validation, now we can return the execution tools
+
+      const taskMap = new Map(Object.entries(tasks))
+
+      const emitter = typedEmitter<{
+        workflowStart: WorkflowStartArgs<W>
+        taskStart: TaskStartArgs<W>
+        taskFinish: TaskFinishArgs<W>
+        taskThrow: TaskThrowArgs<W>
+        taskSkip: TaskSkipArgs<W>
+        workflowFinish: WorkflowFinishArgs<W>
+      }>()
+
+      const runWorkflow = async (context: W[`context`]) => {
+        // Before we start, set up listeners for tasks, to track dependencies
+        const listenForTaskResult = (id: TaskId<W>) => {
+          return new Promise((resolve) => {
+            emitter.on(`taskFinish`, ({ id: finishedTaskId, result }) => {
+              if (finishedTaskId === id) resolve(result)
+            })
+          })
+        }
+
+        // Keep track of the pending task results
+        const taskResultPromiseMap = new Map<
+          TaskId<W>,
+          Promise<TaskFinishArgs<W>[`result`]>
+        >()
+
+        // Set up the listeners in the map, so that we can await them later on
+        for (const taskId of taskOrder) {
+          taskResultPromiseMap.set(
+            taskId,
+            listenForTaskResult(taskId) as Promise<any>,
+          )
+        }
+
+        const getTaskResult = async (id: TaskId<W>) => {
+          return await taskResultPromiseMap.get(id)
+        }
+        emitter.emit(`workflowStart`, { taskOrder, context })
+
+        const taskFinishEvents = new Map<TaskId<W>, TaskFinishArgs<W>>()
+        const tasksSkipped = new Set<TaskId<W>>()
+        const tasksErrored = new Set<TaskId<W>>()
+
+        const taskPromises: Promise<any>[] = []
+        for (const id of taskOrder) {
+          const erroredDependencies = []
+          const skippedDependencies = []
+
+          for (const dep of taskMap.get(id)!.dependencies) {
+            if (tasksErrored.has(dep)) erroredDependencies.push(dep)
+            else if (tasksSkipped.has(dep)) skippedDependencies.push(dep)
+          }
+
+          if (
+            erroredDependencies.length > 0 ||
+            skippedDependencies.length > 0
+          ) {
+            const event = {
+              id,
+              erroredDependencies,
+              skippedDependencies,
+            }
+            tasksSkipped.add(id)
+            emitter.emit(`taskSkip`, event)
+          } else {
+            emitter.emit(`taskStart`, { id })
+
+            try {
+              taskPromises.push(
+                taskMap
+                  .get(id)!
+                  .run({
+                    // @ts-expect-error
+                    getTaskResult,
+                    context,
+                  })
+                  .then((result) => {
+                    const event = { id, result }
+                    taskFinishEvents.set(id, event)
+                    emitter.emit(`taskFinish`, event)
+                    return result
+                  })
+                  .catch((error) => {
+                    throw error
+                  }),
+              )
+            } catch (error) {
+              tasksErrored.add(id)
+
+              if (error instanceof Error) {
+                emitter.emit(`taskThrow`, { id, error })
+              } else {
+                emitter.emit(`taskThrow`, {
+                  id,
+                  error: new Error(String(error)),
+                })
+              }
+            }
           }
         }
-      }
 
-      // construct a graph so we can detect cycles and topo-sort
+        await Promise.all(taskPromises)
 
-      const graph = Graph()
-
-      for (const task of taskList) {
-        graph.addNode(task.id)
-        for (const dep of task.dependencies) {
-          // missing nodes are added implicitly so order doesn't matter
-          graph.addEdge(task.id, dep)
+        const ret = {
+          tasksFinished: [...taskFinishEvents.keys()],
+          tasksErrored: [...tasksErrored],
+          tasksSkipped: [...tasksSkipped],
         }
+
+        emitter.emit(`workflowFinish`, ret)
+        return ret
       }
 
-      if (graph.hasCycle()) throw new Error(`Task graph has a cycle`)
+      return {
+        taskOrder,
+        emitter,
+        runWorkflow,
+      }
+    },
+
+    buildSerialWorkflow: (options: { selectedTasks?: TaskId<W>[] } = {}) => {
+      const graph = validateAndBuildGraph()
 
       const taskOrder: TaskId<W>[] = graph
         .topologicalSort(options.selectedTasks, true)
@@ -173,8 +307,7 @@ type TaskFor<W extends UnknownWorkflowDefinition> = Task<
 
 type TaskId<W extends UnknownWorkflowDefinition> = string & keyof W[`returns`]
 
-// TS says this needs to be exported because it's named in the type of makeTaskFunction
-export interface Task<
+interface Task<
   W extends UnknownWorkflowDefinition,
   Id extends TaskId<W>,
   DepId extends TaskId<W>,
@@ -192,7 +325,7 @@ interface TaskRunContext<
   WD extends UnknownWorkflowDefinition,
   DepId extends TaskId<WD>,
 > {
-  getTaskResult: <D extends DepId>(id: D) => WD[`returns`][D]
+  getTaskResult: <D extends DepId>(id: D) => Promise<WD[`returns`][D]>
   context: WD[`context`]
 }
 
